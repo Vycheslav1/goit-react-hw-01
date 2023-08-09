2a09d00edb6309634964b795e760501363eb6174
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-08-09 10:04:32 +0300

    Add files via upload

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+
+import {
+  Container,
+  Title,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  Label,
+  Quantity,
+  Li,
+} from './ProfileStyles.js';
+
+const Profile = ({ title, username, tag, location, avatar, stats }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Description>
+      <Avatar src={avatar} alt="User avatar" />
+      <Name>{username}</Name>
+      <Tag>@{tag}</Tag>
+      <Location>{location}</Location>
+    </Description>
+
+    <Stats>
+      <Li>
+        <Label>Followers</Label>
+        <Quantity>{stats.followers}</Quantity>
+      </Li>
+      <Li>
+        <Label>Views</Label>
+        <Quantity>{stats.views}</Quantity>
+      </Li>
+      <Li>
+        <Label>Likes</Label>
+        <Quantity>{stats.likes}</Quantity>
+      </Li>
+    </Stats>
+  </Container>
+);
+
+Profile.propTypes = {
+  title: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number.isRequired),
+};
+
+export { Profile };
